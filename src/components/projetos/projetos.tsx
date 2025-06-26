a8b0f02b78a50f8e675caee60e0f6d5dfbd13aff
GabrielCM16 <gabrielcostademoraes13@gmail.com>
2025-06-26 15:09:04 -0300

    arrumando imagens dos projetos e textos

--- src/components/projetos/projetos.tsx
+++ src/components/projetos/projetos.tsx
@@ -1,13 +1,14 @@
 import React, { useEffect, useState } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
-import "./projetos.css"; 
+import "./projetos.css";
 
 import gamejam from './../../assets/images/projects/GameJam.png';
-import lixo from './../../assets/images/projects/Lixo.png';
+import lixo from './../../assets/images/projects/lixo.png';
 import divulgacao from './../../assets/images/projects/DivulgacaoCurso.png';
-import docom from './../../assets/images/projects/Intagram1.png';
-import minicurso from './../../assets/images/projects/MiniCursoProgramacao.png';
-
+import docom from './../../assets/images/projects/insta.png';
+import minicurso from './../../assets/images/projects/minicurso.png';
+import agasalho from './../../assets/images/projects/Agasalho.png';
+import recepcao from './../../assets/images/projects/recepcao.png';
 
 const Projetos: React.FC = () => {
     const [mobile, setMobile] = useState(false);
@@ -17,11 +18,11 @@ const Projetos: React.FC = () => {
     }, []);
 
     return (
-        <main className="container my-5 projetos-container">
+        <main className="container px-3 my-5 projetos-container">
 
-            {/* EXTENSÃO */}
+            {/* PROJETOS */}
             <section className="text-center my-5">
-                <h1 className="display-1 mt-5">EXTENSÃO</h1>
+                <h1 className="display-1 mt-5">PROJETOS</h1>
                 <h2 className="display-4 mt-5">A&B Game Jam</h2>
                 <p className="fs-4 mx-auto" style={{ maxWidth: 1100 }}>
                     A A&B Game Jam é um evento anual organizado pelo PETComp como parte do projeto de Organização de Eventos.
@@ -46,12 +47,13 @@ const Projetos: React.FC = () => {
                 </div>
 
 
-                <img
-                    src={gamejam}	
-                    alt="Logo Petcomp"
-                    className="img-fluid mt-4"
-                    style={{ maxWidth: 700, height: "auto" }}
-                />
+                <div className="mx-auto" style={{ maxWidth: 700 }}>
+                    <img
+                        src={gamejam}
+                        alt="Logo Petcomp"
+                        className="img-fluid w-100 mt-4"
+                    />
+                </div>
             </section>
 
             {/* COLETA DE LIXO ELETRÔNICO */}
@@ -61,12 +63,14 @@ const Projetos: React.FC = () => {
                     O grupo disponibiliza lixeiras exclusivas para a coleta de lixo eletrônico no Campus da Unioeste Cascavel.
                     Essa iniciativa visa principalmente auxiliar a comunidade na destinação adequada desse tipo de resíduo.
                 </p>
-                <img
-                    src={lixo}
-                    alt="Logo Petcomp"
-                    className="img-fluid mt-3"
-                    style={{ maxWidth: 600, height: "auto" }}
-                />
+                <div className="mx-auto" style={{ maxWidth: 600 }}>
+                    <img
+                        src={lixo}
+                        alt="Logo Petcomp"
+                        className="img-fluid w-100 mt-4"
+                    />
+                </div>
+
             </section>
 
             {/* DIVULGAÇÃO DO CURSO */}
@@ -78,12 +82,14 @@ const Projetos: React.FC = () => {
                     Essa iniciativa visa não apenas informar a comunidade sobre as oportunidades educacionais disponíveis,
                     mas também atrair potenciais interessados na área.
                 </p>
-                <img
-                    src={divulgacao}
-                    alt="Logo Petcomp"
-                    className="img-fluid mt-3"
-                    style={{ maxWidth: 800, height: "auto" }}
-                />
+                <div className="mx-auto" style={{ maxWidth: 800 }}>
+                    <img
+                        src={divulgacao}
+                        alt="Logo Petcomp"
+                        className="img-fluid w-100 mt-4"
+                    />
+                </div>
+
             </section>
 
             {/* DOCOM */}
@@ -101,13 +107,15 @@ const Projetos: React.FC = () => {
                         Instagram
                     </a>.
                 </p>
+                <div className="mx-auto" style={{ maxWidth: 600 }}>
+                    <img
+                        src={docom}
+                        alt="Logo Petcomp"
+                        className="img-fluid w-100 mt-4"
+                    />
+                </div>
 
-                <img
-                    src={docom}
-                    alt="Logo Petcomp"
-                    className="img-fluid mt-3"
-                    style={{ maxWidth: 600, height: "auto" }}
-                />
+
             </section>
 
             {/* PETCODE */}
@@ -119,55 +127,43 @@ const Projetos: React.FC = () => {
 
                 <div className="row justify-content-center align-items-center mt-4" style={{ maxWidth: 1000, margin: "0 auto" }}>
                     <div className="col-md-6">
-                        <p className="fs-4 fw-bold">Minicurso de Programação:</p>
+                        <p className="fs-4 fw-bold">Minicurso de Criptografia Aplicada:</p>
                         <p className="fs-4">
-                            Em 2024, o PETComp promoverá um Minicurso de Programação aberto à comunidade interna e externa da Unioeste.
-                            O objetivo principal deste minicurso é introduzir conceitos fundamentais de lógica de programação.
-                            Este ano, o curso será ministrado utilizando a linguagem Python.
+                            Em 2025, o PETComp promoverá um Minicurso de Criptografia Aplicada aberto à comunidade interna e externa da Unioeste.
+                            O objetivo principal deste minicurso é apresentar conceitos fundamentais de criptografia, com foco em aplicações práticas no contexto da segurança da informação.
                         </p>
+
                     </div>
                     <div className="col-md-6 text-center">
-                        <img
-                            src={minicurso}
-                            alt="logo pet"
-                            className="img-fluid"
-                            style={{ maxWidth: 400, height: "auto" }}
-                        />
+                        <div className="mx-auto" style={{ maxWidth: 400 }}>
+                            <img
+                                src={minicurso}
+                                alt="Logo Petcomp"
+                                className="img-fluid w-100 mt-4"
+                            />
+                        </div>
+
                     </div>
                 </div>
 
-                <div className="row justify-content-center align-items-center mt-5" style={{ maxWidth: 1000, margin: "0 auto" }}>
-                    <div className="col-md-6">
-                        <p className="fs-4 fw-bold">Monitoria de Algoritmos:</p>
-                        <p className="fs-4">
-                            Todas as segundas-feiras, às 17:00h, a equipe realiza monitorias da disciplina de algoritmos destinadas aos estudantes do 1° ano de Ciência da Computação da Unioeste de Cascavel.
-                            O propósito principal deste projeto é auxiliar os alunos, fornecendo suporte para esclarecer suas dúvidas relacionadas à disciplina.
-                        </p>
-                    </div>
-                    <div className="col-md-6 text-center">
-                        <img
-                            src="/images/projects/MonitoriaAlgoritmos.png"
-                            alt="logo pet"
-                            className="img-fluid"
-                            style={{ maxWidth: 400, height: "auto" }}
-                        />
-                    </div>
-                </div>
+
             </section>
 
             {/* PETVOLUNTÁRIO */}
             <section className="text-center my-5">
                 <h2 className="display-4">PETVoluntário</h2>
                 <p className="fs-4 mx-auto" style={{ maxWidth: 1200 }}>
-                    Este projeto visa realizar atividades que beneficiem a comunidade externa, tais como doação de sangue, arrecadação de itens materiais para aqueles que necessitam, e outras iniciativas com propósito semelhante.
+                    Este projeto contempla ações solidárias voltadas à comunidade externa, como campanhas de doação de sangue, arrecadação de agasalhos e outros itens essenciais para pessoas em situação de vulnerabilidade, promovendo cidadania e responsabilidade social.
                 </p>
-                <img
-                    src="/images/projects/Volutariado.png"
-                    alt="Logo Petcomp"
-                    className="img-fluid mt-3"
-                    style={{ maxWidth: 600, height: "auto" }}
-                />
+                <div className="mx-auto" style={{ maxWidth: 600 }}>
+                    <img
+                        src={agasalho}
+                        alt="Campanha PETVoluntário"
+                        className="img-fluid w-100 mt-3"
+                    />
+                </div>
             </section>
+
 
             {/* RECEPÇÃO DOS CALOUROS */}
             <section className="text-center my-5 mb-5">
@@ -176,16 +172,17 @@ const Projetos: React.FC = () => {
                     Para facilitar a integração dos novos calouros ao curso de Ciência da Computação na universidade,
                     o PET realiza uma apresentação abordando o programa, o funcionamento do sistema universitário e informações essenciais que os alunos necessitam conhecer.
                 </p>
-                <img
-                    src="/images/projects/RecepcaoDosCalourouAno1500.jpg"
-                    alt="Logo Petcomp"
-                    className="img-fluid mt-3"
-                    style={{ maxWidth: 800, height: "auto" }}
-                />
+                <div className="mx-auto" style={{ maxWidth: 800 }}>
+                    <img
+                        src={recepcao}
+                        alt="Campanha PETVoluntário"
+                        className="img-fluid w-100 mt-3"
+                    />
+                </div>
             </section>
 
 
-        </main>
+        </main >
     );
 };
 
